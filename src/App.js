b32c0aeb2e07942a45764e76987872a6d2c7d2ad
redoprogrammer <redoprogrammer@gmail.com>
2023-03-10 10:40:43 +0700

    define table columns

--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import './App.css';
 import {Table} from 'antd';
 
 function App() {
+  const columns = [
+    {
+      key:1,
+      title:'ID',
+      dataIndex:'id'
+    },
+    {
+      key:2,
+      title:'Name',
+      dataIndex:'name'
+    },
+    {
+      key:3,
+      title:'Email',
+      dataIndex:'email'
+    },
+    {
+      key:4,
+      title:'Address',
+      dataIndex:'address'
+    }
+  ]
+
+  
   return (
     <div className="App">
-     
+      <header>
+        <Table
+          columns={columns}
+          dataSource={}
+        >
+
+        </Table>
+      </header>
     </div>
   );
 }
